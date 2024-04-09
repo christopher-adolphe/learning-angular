3f18925fc0fff22c77496deae4760188c3e50bf4
cadolphe <christopher.adolphe@gmail.com>
2024-04-09 06:22:14 +0400

    remove firebaseAPIKey from recipe-app project

--- recipe-app-with-ngrx/src/environments/environment.prod.ts
+++ recipe-app-with-ngrx/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  firebaseAPIKey: 'AIzaSyAz6xu_sSCmtb568XkRHsDFcpWmKWlhd9w'
+  firebaseAPIKey: ''
 };
--- recipe-app-with-ngrx/src/environments/environment.ts
+++ recipe-app-with-ngrx/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebaseAPIKey: 'AIzaSyAz6xu_sSCmtb568XkRHsDFcpWmKWlhd9w'
+  firebaseAPIKey: ''
 };
 
 /*
