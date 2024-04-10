6f8242c753a6cbe7626af8ff9e872003694b76cd
cadolphe <christopher.adolphe@gmail.com>
2024-04-10 17:24:31 +0400

    convert root component to standalone and change bootstrap function

--- standalone-components-basics/src/main.ts
+++ standalone-components-basics/src/main.ts
@@ -1,12 +1,20 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+// Using the `bootstrapApplication()` function from `'@angular/platform-browser'`
+// to bootstrap the Angular application with the root `AppComponent` now
+// converted to a standalone component
+bootstrapApplication(AppComponent);
